29d0c947b70f0faecafc1e7baf5dfbfc5f723ce3
TheBestShooter <rnolascoc@uni.pe>
2020-01-11 17:25:13 -0500

    Finalizando Vue

---
+++ vuejs/parte-1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Hola mundo!
+        <button @click="saludar">Chupetin</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    methods: {
+        saludar: function(){
+            alert('hola');
+        }
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
--- vuejs/parte-1/src/app.js
+++ vuejs/parte-1/src/app.js
@@ -1,1 +1,6 @@
-alert('hello');
+import Vue from 'vue'
+import App from './App.vue'
+new Vue({
+    el:'#app',
+    render: h => h(App)
+})
