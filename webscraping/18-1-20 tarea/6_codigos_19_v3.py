32261e7345c8ad5be85fb847cd63e63f1fea56bf
TheBestShooter <rnolascoc@uni.pe>
2020-01-24 19:38:37 -0500

    poniendo para n anios

--- webscraping/18-1-20 tarea/6_codigos_19_v3.py
+++ webscraping/18-1-20 tarea/6_codigos_19_v3.py
@@ -14,30 +14,32 @@ def obtenerCodigoUNI(codigoUNI):
     for i in range(len(cod)):
         suma += int(cod[i]) * arr[i]
     return (cod + chr(65 + suma % 11))
-
-i=0
-while i<10000:
-    codigoesp=""
-    nombreesp=""
-    codigo=obtenerCodigoUNI(2019*10000 + i)
-    url = "https://www.orce.uni.edu.pe/detaalu.php?id="+codigo+"&op=detalu"
-    response = r.get(url, verify=False)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
-    nombrerow = soup.tbody.find_all('tr')[5]
-    codrow = soup.tbody.find_all('tr')[9] # bs4.element.Tag
-    nombrecol = nombrerow.find_all('td')
-    codcol = codrow.find_all('td') # bs4.element.ResultSet
-    nombre = str(nombrecol[1].string)
-    cod = str(codcol[1].string) #bs4.element.String to str
-    if(nombre!="None"):
-        for x in json:
-            if(x['nombre']==cod): #Ambos son del mismo tipo
-                codigoesp = x['codigo']
-                break
-        print(codigo + " " + codigoesp + " " + nombre)
-        i+=1
-    elif(nombre=="None" and i%500!=0):
-        i=((i//500)+1)*500
-    else:
-        i+=1
+inicio = int(input("Ingrese el año inicial: "))
+fin = int(input("Ingrese el año final:"))
+for anio in range(inicio, fin+1):
+    i=0
+    while i<10000:
+        codigoesp=""
+        nombreesp=""
+        codigo=obtenerCodigoUNI(anio*10000 + i)
+        url = "https://www.orce.uni.edu.pe/detaalu.php?id="+codigo+"&op=detalu"
+        response = r.get(url, verify=False)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, 'html.parser')
+        nombrerow = soup.tbody.find_all('tr')[5]
+        codrow = soup.tbody.find_all('tr')[9] # bs4.element.Tag
+        nombrecol = nombrerow.find_all('td')
+        codcol = codrow.find_all('td') # bs4.element.ResultSet
+        nombre = str(nombrecol[1].string)
+        cod = str(codcol[1].string) #bs4.element.String to str
+        if(nombre!="None"):
+            for x in json:
+                if(x['nombre']==cod): #Ambos son del mismo tipo
+                    codigoesp = x['codigo']
+                    break
+            print(codigo + " " + codigoesp + " " + nombre)
+            i+=1
+        elif(nombre=="None" and i%500!=0):
+            i=((i//500)+1)*500
+        else:
+            i+=1
