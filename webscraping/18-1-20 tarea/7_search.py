fb0ddfdd3b9d01b567fdd6a94db70dd96cc74d90
TheBestShooter <rnolascoc@uni.pe>
2020-02-08 13:14:04 -0500

    Ajustando search

--- webscraping/18-1-20 tarea/7_search.py
+++ webscraping/18-1-20 tarea/7_search.py
@@ -3,8 +3,16 @@ f1 = f.readlines()
 for i in range (0,len(f1)):
     x=f1[i].split()
     num = x[0][:-1]
+    """
     if(int(num)>19950000 and int(num)<20200000):
         if(x[1]=='I1' or x[1]=='I2'):
             y=x[2].split('-')
             if(y[0]=='LOPEZ'):
                 print(x)
+    """
+    if(int(num)>20050000 and int(num)<20160000):
+        if(x[1]=='I1' or x[1]=='I2'):
+            y=x[2].split('-')
+            if(len(y)>=3):
+                if(y[2]=='ROGER'):
+                    print(x)
